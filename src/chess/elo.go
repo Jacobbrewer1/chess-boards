2c537a1a3ab0f8b858d69606c4c1c16eb264a3db
Jacob <Jacob.brewer@unity3d.com>
2023-03-10 18:24:52 +0000

    Enhancing unit tests and refactoring the method to be better named

--- src/chess/elo.go
+++ src/chess/elo.go
@@ -5,25 +5,29 @@ import (
 	"math"
 )
 
+const EloInitialValue = 1500
+const EloDefaultConstant = 30
+
 // probability returns the probability of rating1 winning
-func probability(rating1, rating2 float64) float64 {
-	return float64(1.0) * 1.0 / (1 + 1.0*math.Pow(10, 1.0*(rating1-rating2)/400))
+func probability(ratingA, ratingB float64) float64 {
+	return float64(1.0) * 1.0 / (1 + 1.0*math.Pow(10, 1.0*(ratingA-ratingB)/400))
 }
 
-// NewEloRatings returns the new player a rating and the new rating for player b
-func NewEloRatings(ratingA, ratingB float64, constant int, playerAWon custom.Bool) (float64, float64) {
-	var newRatingA, newRatingB float64
+// NewElo is a wrapper func for the NewEloWithConstant func, however we use the EloDefaultConstant value
+func NewElo(currentRating, opponentRating float64, playerWon custom.Bool) float64 {
+	return NewEloWithConstant(currentRating, opponentRating, EloDefaultConstant, playerWon)
+}
 
-	probabilityPlayerB := probability(ratingA, ratingB)
-	probabilityPlayerA := probability(ratingB, ratingA)
+func NewEloWithConstant(currentRating, opponentRating float64, constant int, playerWon custom.Bool) float64 {
+	var newRating float64
 
-	if playerAWon {
-		newRatingA = ratingA + float64(constant)*(1-probabilityPlayerA)
-		newRatingB = ratingB + float64(constant)*(0-probabilityPlayerB)
+	winningProbability := probability(opponentRating, currentRating)
+
+	if playerWon {
+		newRating = currentRating + float64(constant)*(1-winningProbability)
 	} else {
-		newRatingA = ratingA + float64(constant)*(0-probabilityPlayerA)
-		newRatingB = ratingB + float64(constant)*(1-probabilityPlayerB)
+		newRating = currentRating + float64(constant)*(0-winningProbability)
 	}
 
-	return math.Round(newRatingA*1.0) / 1.0, math.Round(newRatingB*1.0) / 1.0
+	return math.Round(newRating*1.0) / 1.0
 }
--- src/chess/elo_test.go
+++ src/chess/elo_test.go
@@ -5,20 +5,67 @@ import (
 	"testing"
 )
 
-func TestCalculateNew(t *testing.T) {
-	ra := 1200
-	rb := 1000
-	k := 30
-	gotA, gotB := NewEloRatings(float64(ra), float64(rb), k, custom.Bool(true))
-
-	//1207.21 Rb = 992.792
-	if gotA != float64(1207) {
-		t.Errorf("gotA = %f, expected %d", gotA, 1207)
-		return
+func TestNewElo(t *testing.T) {
+	tests := []struct {
+		name                string
+		currentPlayerRating float64
+		opponentRating      float64
+		playerWon           bool
+		expected            float64
+	}{
+		{"Two new players, winner: current", EloInitialValue, EloInitialValue, true, 1515},
+		{"Two new players, winner: opponent", EloInitialValue, EloInitialValue, false, 1485},
+		{"New vs established strong player, winner: current", EloInitialValue, 1800, true, 1525},
+		{"New vs established strong player, winner: opponent", EloInitialValue, 1800, false, 1495},
+		{"Established strong player vs new, winner: current", 1800, EloInitialValue, true, 1805},
+		{"Established strong player vs new, winner: opponent", 1800, EloInitialValue, false, 1775},
+		{"New vs established player, winner: current", EloInitialValue, 1800, true, 1525},
+		{"New vs established player, winner: opponent", EloInitialValue, 1800, false, 1495},
+		{"Established weaker player vs new, winner: opponent", 900, EloInitialValue, true, 929},
+		{"Established weaker player vs new, winner: opponent", 900, EloInitialValue, false, 899},
+		{"New vs established weaker player, winner: opponent", EloInitialValue, 900, true, 1501},
+		{"New vs established weaker player, winner: opponent", EloInitialValue, 900, false, 1471},
 	}
 
-	if gotB != 993 {
-		t.Errorf("gotB = %f, expected %d", gotB, 993)
-		return
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotA := NewElo(tt.currentPlayerRating, tt.opponentRating, custom.Bool(tt.playerWon))
+			if gotA != tt.expected {
+				t.Errorf("got = %f, expected %f", gotA, tt.expected)
+				return
+			}
+		})
+	}
+}
+
+func TestNewEloWithConstant(t *testing.T) {
+	tests := []struct {
+		name                string
+		currentPlayerRating float64
+		opponentRating      float64
+		constant            int
+		playerWon           bool
+		expected            float64
+	}{
+		// constant of 20
+		{"current: 1200, opponent: 1000, const: 200, Winner: current", 1200, 1000, 20, true, 1205},
+		{"current: 1200, opponent: 1000, const: 200, Winner: opponent", 1200, 1000, 20, false, 1185},
+		{"current: 1000, opponent: 1200, const: 200, Winner: current", 1000, 1200, 20, true, 1015},
+		{"current: 1000, opponent: 1200, const: 200, Winner: opponent", 1000, 1200, 20, false, 995},
+		// constant of 30
+		{"current: 1200, opponent: 1000, const: 30, Winner: current", 1200, 1000, 30, true, 1207},
+		{"current: 1200, opponent: 1000, const: 30, Winner: opponent", 1200, 1000, 30, false, 1177},
+		{"current: 1000, opponent: 1200, const: 30, Winner: current", 1000, 1200, 30, true, 1023},
+		{"current: 1000, opponent: 1200, const: 30, Winner: opponent", 1000, 1200, 30, false, 993},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotA := NewEloWithConstant(tt.currentPlayerRating, tt.opponentRating, tt.constant, custom.Bool(tt.playerWon))
+			if gotA != tt.expected {
+				t.Errorf("got = %f, expected %f", gotA, tt.expected)
+				return
+			}
+		})
 	}
 }
